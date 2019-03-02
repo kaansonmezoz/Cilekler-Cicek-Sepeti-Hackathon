ea95f104ecf15d9511e6009f87bc3a4f39fba978
Kaan <ksonmezoz@outlook.com>
2019-03-02 17:26:43 +0300

    hadi bismillah

--- FileOperations/Excel/ExcelParser.cs
+++ FileOperations/Excel/ExcelParser.cs
@@ -104,7 +104,7 @@ namespace FileOperations.Excel
             {
                 foreach (Cell cell in row.cellList)
                 {
-                    Console.WriteLine(Convert.ToInt32(cell.stringIndex)];
+                    Console.WriteLine(valueList[Convert.ToInt32(cell.stringIndex)]);
                     cell.value = valueList[Convert.ToInt32(cell.stringIndex)].Replace(".", ",");
                     Console.WriteLine(cell.value);
                 }
