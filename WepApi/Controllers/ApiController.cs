a4c5785a49efb71a90c10a81b0663f81d9b21d31
Kaan <ksonmezoz@outlook.com>
2019-03-02 16:46:44 +0300

    Longitude ve latitude string olarak gonderiliyor

--- WepApi/Controllers/ApiController.cs
+++ WepApi/Controllers/ApiController.cs
@@ -106,7 +106,7 @@ namespace Api.Controllers
 
             return orderList.Select((order) => new ControllerEntities.Order{
                 orderNumber = order.orderNumber,
-                latitude = Convert.ToString(order.latitude),
+                latitude = Convert.ToString(order.latitude).Replace(".", ","),
                 longitude = Convert.ToString(order.longitude)
             }).ToList();
         }
