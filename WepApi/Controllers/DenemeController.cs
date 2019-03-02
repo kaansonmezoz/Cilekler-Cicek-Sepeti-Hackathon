51f7db51e0a9de1f1835162530748f44eecb7628
Kaan <ksonmezoz@outlook.com>
2019-03-02 14:23:20 +0300

    Ayakta mi diye bakmak icin basit api yazildi

---
+++ WepApi/Controllers/DenemeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DenemeController : ControllerBase
+    {
+        [HttpGet]
+        public string IsAlive() {
+            return "SDKJFASKLDJFLKASDJFKLASJDF";
+        }
+
+
+    }
+}
